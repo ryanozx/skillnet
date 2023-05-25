97f8d47120b4cd1adaa502233c3cb61406de1db0
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-05-25 21:34:43 +0800

    Implement SignUp button to redirect to Signup page

--- frontend/components/NavBar/SignUpButton.jsx
+++ frontend/components/NavBar/SignUpButton.jsx
@@ -1,20 +1,22 @@
+import Link from 'next/link';
 import { Button } from '@chakra-ui/button';
 
 export default function SignUpButton() {
     return (
-        <Button
-            as={'a'}
-            display={'inline-flex'}
-            fontSize={'sm'}
-            fontWeight={600}
-            color={'white'}
-            bg={'blue.400'}
-            href={'#'}
-            _hover={{
-                bg: 'blue.300',
-            }}
-        >
-            Sign Up
-        </Button>
+        <Link href="/signup">
+            <Button
+                as={'a'}
+                display={'inline-flex'}
+                fontSize={'sm'}
+                fontWeight={600}
+                color={'white'}
+                bg={'blue.400'}
+                _hover={{
+                    bg: 'blue.300',
+                }}
+            >
+                Sign Up
+            </Button>
+        </Link>
     );
 }
