7e3e3bb186dca641a8592434f4382f49cbad5f8c
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-05-26 21:26:11 +0800

    fix hudration error with signup and login buttons

--- frontend/components/NavBar/LogInButton.jsx
+++ frontend/components/NavBar/LogInButton.jsx
@@ -1,18 +1,23 @@
-import Link from 'next/link';
 import { Button } from '@chakra-ui/react';
+import { useRouter } from 'next/router';
 
 export default function LogInButton() {
+    const router = useRouter();
+
+    const handleLogin = () => {
+        router.push('/login');
+    };
+
     return (
-        <Link href="/login">
-            <Button
-                fontSize={'sm'}
-                fontWeight={400}
-                color={'blackAlpha.900'}
-                variant={'link'}
-                as={'a'}
-            >
-                Log in
-            </Button>
-        </Link>
+        <Button
+            fontSize={'sm'}
+            fontWeight={400}
+            color={'blackAlpha.900'}
+            variant={'link'}
+            onClick={handleLogin}
+        >
+        Log in
+        </Button>
     );
 }
+
--- frontend/components/NavBar/SignUpButton.jsx
+++ frontend/components/NavBar/SignUpButton.jsx
@@ -1,22 +1,27 @@
-import Link from 'next/link';
 import { Button } from '@chakra-ui/button';
+import { useRouter } from 'next/router';
 
 export default function SignUpButton() {
-    return (
-        <Link href="/signup">
-            <Button
-                as={'a'}
-                display={'inline-flex'}
-                fontSize={'sm'}
-                fontWeight={600}
-                color={'white'}
-                bg={'blue.400'}
-                _hover={{
-                    bg: 'blue.300',
-                }}
-            >
-                Sign Up
-            </Button>
-        </Link>
-    );
+  const router = useRouter();
+
+  const handleSignUp = () => {
+    router.push('/signup');
+  };
+
+  return (
+    <Button
+      as={'a'}
+      display={'inline-flex'}
+      fontSize={'sm'}
+      fontWeight={600}
+      color={'white'}
+      bg={'blue.400'}
+      _hover={{
+        bg: 'blue.300',
+      }}
+      onClick={handleSignUp}
+    >
+      Sign Up
+    </Button>
+  );
 }
