84119fbdba3b7bce4fe785cbbdc019a259794de0
Ryan Ong <59072730+ryanozx@users.noreply.github.com>
2023-06-10 16:32:01 +0800

    Set up authentication system and refactored models

--- backend/database/posts.go
+++ backend/database/posts.go
@@ -1,53 +1,90 @@
 package database
 
 import (
+	"errors"
+
 	"github.com/ryanozx/skillnet/models"
 	"gorm.io/gorm"
 )
 
-func GetPosts(db *gorm.DB) ([]models.Post, error) {
-	var posts []models.PostSchema
-	query := db.Find(&posts)
-	output := models.ConvertToPosts(posts)
+var ErrNotOwner = errors.New("unauthorised action")
+
+func GetPosts(db *gorm.DB) ([]models.PostView, error) {
+	var posts []models.Post
+	var postViews []models.PostView
+
+	// Retrieve all posts from database
+	query := db.Preload("User").Find(&posts)
 	if err := query.Find(&posts).Error; err != nil {
-		return output, err
+		return nil, err
 	}
-	return output, nil
+
+	// Fill in user details for each post using userID of post creator
+	for _, post := range posts {
+		post := bindUserToPost(&post)
+		postViews = append(postViews, *post)
+	}
+	return postViews, nil
 }
 
-func CreatePost(db *gorm.DB, postInput *models.PostInput) (*models.Post, error) {
-	newPostSchema := postInput.ConvertToPostSchema()
-	result := db.Create(newPostSchema)
-	newPost := newPostSchema.ConvertToPost()
+func CreatePost(db *gorm.DB, post *models.Post) (*models.PostView, error) {
+	result := db.Create(post)
 	if err := result.Error; err != nil {
-		return newPost, err
+		return nil, err
 	}
+	newPost := bindUserToPost(post)
 	return newPost, nil
 }
 
-func GetPostSchemaByID(db *gorm.DB, id string) (*models.PostSchema, error) {
-	postSchema := models.PostSchema{}
-	err := db.First(&postSchema, id).Error
+func GetPostByID(db *gorm.DB, id string) (*models.PostView, error) {
+	post := models.Post{}
+	err := db.First(&post, id).Error
 	if err != nil {
-		return &postSchema, err
+		return nil, err
 	}
-	return &postSchema, nil
+	postView := bindUserToPost(&post)
+	return postView, nil
 }
 
-func UpdatePost(db *gorm.DB, updateInput *models.PostInput, id string) (*models.PostSchema, error) {
-	originalPost, err := GetPostSchemaByID(db, id)
+func UpdatePost(db *gorm.DB, post *models.Post, id string) (*models.PostView, error) {
+	originalPostView, err := GetPostByID(db, id)
 	if err != nil {
-		return originalPost, err
+		return nil, err
 	}
-	err = db.Model(originalPost).Updates(updateInput).Error
-	return originalPost, err
+	originalPost := originalPostView.Post
+	userID := originalPost.UserID.String()
+	if err := checkUserIsOwner(originalPostView, userID); err != nil {
+		return nil, err
+	}
+	err = db.Model(originalPost).Updates(post).Error
+	if err != nil {
+		return nil, err
+	}
+	postView := bindUserToPost(&originalPost)
+	return postView, nil
 }
 
-func DeletePost(db *gorm.DB, id string) error {
-	postSchema, err := GetPostSchemaByID(db, id)
+func DeletePost(db *gorm.DB, id string, userID string) error {
+	postView, err := GetPostByID(db, id)
 	if err != nil {
 		return err
 	}
-	err = db.Delete(&postSchema).Error
+	if err := checkUserIsOwner(postView, userID); err != nil {
+		return err
+	}
+	post := postView.Post
+	err = db.Delete(&post).Error
 	return err
+}
+
+func bindUserToPost(post *models.Post) *models.PostView {
+	postView := post.PostView()
+	return postView
+}
+
+func checkUserIsOwner(postView *models.PostView, userID string) error {
+	if postView.Post.UserID.String() != userID {
+		return ErrNotOwner
+	}
+	return nil
 }
--- backend/models/user.go
+++ backend/models/user.go
@@ -3,58 +3,75 @@ package models
 import (
 	"time"
 
+	"gorm.io/gorm"
+
 	"github.com/google/uuid"
 )
 
+// UserMinimal contains the bare essential user details that can
+// be displayed next to posts, comments, etc.
 type UserMinimal struct {
-	ID         uuid.UUID
 	Name       string
-	ProfileURL string
+	URL        string `gorm:"-:all"`
 	ProfilePic string
 }
 
+// UserCredentials handles login/signup information
 type UserCredentials struct {
-	Username string
-	Password string
+	Username string `gorm:"unique; not null"`
+	Password string `gorm:"not null" json:"-"`
 }
 
-type User struct {
-	ID            uuid.UUID `gorm:"type:uuid;primary_key;"`
-	Username      string
-	Password      string
-	Name          string
-	ProfilePicURI string
+// UserView represents the information a visitor to a user's profile page can see
+type UserView struct {
+	UserMinimal   `gorm:"embedded"`
 	Title         string
 	Birthday      time.Time
 	Location      string
 	AboutMe       string
-	// Projects      ProjectsArray
+	ProjectsArray `json:"projects" gorm:"-:all"`
 }
 
-type UserWithSettings struct {
-	User              User `gorm:"embedded"`
-	TitlePrivileges   uint8
-	BirthdayPrivilege uint8
-	LocationPrivilege uint8
-	AboutMePrivilege  uint8
-	ProjectsPrivilege uint8
+// User is the database representation of a user object
+type User struct {
+	ID              string   `gorm:"type:uuid;<-:create;default:uuid_generate_v4()" json:"-"` // UserID will never be revealed to the client; it will never change
+	Profile         UserView `gorm:"embedded"`
+	UserCredentials `gorm:"embedded"`
+	Email           string
 }
 
-func generateTestUser(userID uuid.UUID) *UserMinimal {
-	testUser := UserMinimal{
-		ID:         userID,
-		Name:       "User1",
-		ProfileURL: "www.skillnet.com/user/user1",
-		ProfilePic: "user1PicURL",
+func (userCreds *UserCredentials) NewUser() *User {
+	user := User{
+		UserCredentials: *userCreds,
 	}
-	return &testUser
+	return &user
 }
 
-func (userCreds *UserCredentials) ConvertToUser() *User {
-	newUser := User{
-		ID:       uuid.New(),
-		Username: userCreds.Username,
-		Password: userCreds.Password,
-	}
-	return &newUser
+func (user *User) BeforeCreate(tx *gorm.DB) error {
+	user.ID = uuid.NewString()
+	return nil
+}
+
+func (user *User) AfterFind(tx *gorm.DB) error {
+	user.GenerateProfileURL()
+	return nil
+}
+
+func (user *User) AfterUpdate(tx *gorm.DB) error {
+	user.GenerateProfileURL()
+	return nil
+}
+
+func (user *User) UserMinimal() *UserMinimal {
+	return &user.Profile.UserMinimal
+}
+
+func (profile *UserView) GenerateUserMinimal() *UserMinimal {
+	return &profile.UserMinimal
+}
+
+func (user *User) GenerateProfileURL() {
+	const profileUrlPrefix = "localhost:8080/users/"
+	url := profileUrlPrefix + user.Username
+	user.Profile.URL = url
 }
