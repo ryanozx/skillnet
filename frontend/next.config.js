20f79cd9f226ab278e9a5360e8f84c1f15e06885
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-06-24 22:36:01 +0800

    Make changes to next.config file to solve vercel deployment error

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,3 +1,19 @@
+// // next.config.js
+// const path = require('path');
+// const dotenv = require('dotenv');
+
+// // Resolve the parent directory path
+// const parentDir = path.join(__dirname, '..');
+
+// // Load the .env file from the parent directory
+// const envConfig = dotenv.config({ path: path.join(parentDir, '.env') }).parsed;
+
+// module.exports = {
+//   env: {
+//     BACKEND_BASE_URL: envConfig.BACKEND_HOST + ":" + envConfig.BACKEND_PORT, 
+//   },
+// };
+
 // next.config.js
 const path = require('path');
 const dotenv = require('dotenv');
@@ -5,11 +21,15 @@ const dotenv = require('dotenv');
 // Resolve the parent directory path
 const parentDir = path.join(__dirname, '..');
 
-// Load the .env file from the parent directory
-const envConfig = dotenv.config({ path: path.join(parentDir, '.env') }).parsed;
+// Try to load the .env file from the parent directory
+const dotenvConfig = dotenv.config({ path: path.join(parentDir, '.env') });
+
+// Use the parsed dotenv config if available, otherwise use process.env
+const envConfig = dotenvConfig.error ? process.env : dotenvConfig.parsed;
 
 module.exports = {
   env: {
-    BACKEND_BASE_URL: envConfig.BACKEND_HOST + ":" + envConfig.BACKEND_PORT, 
+    BACKEND_BASE_URL: envConfig.BACKEND_HOST + ":" + envConfig.BACKEND_PORT,
   },
 };
+
