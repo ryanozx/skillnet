213b4c8061d0f7baf2541dca07728555727ec2e1
Ivan <103909775+ivyy-poison@users.noreply.github.com>
2023-06-25 21:16:20 +0800

    Refactor frontend code (#20)
    * Connecting Vercel build with .env variables
    
    ---------
    
    Co-authored-by: Ryan Ong <59072730+ryanozx@users.noreply.github.com>

--- frontend/next.config.js
+++ frontend/next.config.js
@@ -1,19 +1,3 @@
-// // next.config.js
-// const path = require('path');
-// const dotenv = require('dotenv');
-
-// // Resolve the parent directory path
-// const parentDir = path.join(__dirname, '..');
-
-// // Load the .env file from the parent directory
-// const envConfig = dotenv.config({ path: path.join(parentDir, '.env') }).parsed;
-
-// module.exports = {
-//   env: {
-//     BACKEND_BASE_URL: envConfig.BACKEND_HOST + ":" + envConfig.BACKEND_PORT, 
-//   },
-// };
-
 // next.config.js
 const path = require('path');
 const dotenv = require('dotenv');
@@ -29,7 +13,6 @@ const envConfig = dotenvConfig.error ? process.env : dotenvConfig.parsed;
 
 module.exports = {
   env: {
-    BACKEND_BASE_URL: envConfig.BACKEND_HOST + ":" + envConfig.BACKEND_PORT,
+    BACKEND_BASE_URL: envConfig.BACKEND_BASE_URL
   },
 };
-
