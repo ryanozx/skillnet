c1c23a9a4a6371707b8baffab6419847a2835677
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-07-08 15:18:45 +0800

    Shift snapshot tests out of pages directory

--- frontend/__tests__/home.test.tsx
+++ frontend/__tests__/home.test.tsx
@@ -1,6 +1,6 @@
 import React from 'react';
 import { render } from '@testing-library/react';
-import Home from './index';
+import Home from '../pages/index';
 
 jest.mock('next/router', () => require('next-router-mock'));
 jest.mock('../withAuthRedirect', () => ({
--- frontend/components/HomePage/BodyText.tsx
+++ frontend/components/HomePage/BodyText.tsx
@@ -6,6 +6,6 @@ export const BodyText = () => (
         SkillNet aims to fill a gap in the current landscape of university education 
         by providing a platform for students to collaborate on projects, share 
         knowledge, and foster a supportive community that encourages learning and 
-        skill development in various areas. 
+        skill development in various areas.
     </Text>
 );
