d0e2f1683b23382fb192a60c743973c7ff3778d2
ivyy-poison <tan.yuhao.ivan@gmail.com>
2023-07-09 21:48:06 +0800

    Connect Frontend as sub to pub/sub system

--- frontend/components/base/NavBar/NavBar.tsx
+++ frontend/components/base/NavBar/NavBar.tsx
@@ -2,10 +2,11 @@ import {
     Box,
     Flex,
     useBreakpointValue,
+    useToast,
 } from '@chakra-ui/react';
 import DesktopNav from './DesktopNav';
 import MobileNav from './MobileNav';
-import React from 'react';
+import React, { useState, useEffect } from 'react';
 
 interface NavBarProps {
     profilePic: string;
@@ -15,7 +16,43 @@ interface NavBarProps {
 export default function NavBar(props: any) {
     const { profilePic, username } = props;
     const isDesktop = useBreakpointValue({ base: false, lg: true });
+    const toast = useToast();
+
+    const [eventSource, setEventSource] = useState<EventSource | null>(null);
+
+    useEffect(() => {
+        console.log("this is being ran");
+        if (!eventSource) {
+            console.log("this is being ran too");
+            const source = new EventSource(process.env.BACKEND_BASE_URL + '/auth/notifications', {
+                withCredentials: true,
+            });
     
+            source.onmessage = function (event) {
+                // const notification = JSON.parse(event.data);
+                console.log("event: " + event.data);
+                // Display the notification. For simplicity, let's use toast here.
+                toast({
+                    title: "New notification",
+                    description: event.data,
+                    status: "info",
+                    duration: 5000,
+                    isClosable: true,
+                });
+            };
+    
+            source.onerror = function (event) {
+                console.error("EventSource failed:", event);
+            };
+    
+            setEventSource(source);
+        }
+    
+        // When the component unmounts, close the event source
+        return () => {
+            eventSource?.close();
+        };
+    }, [eventSource]);    
     
     return (
         <Box>
